9c79658333b56dd93e8834354a3fd4aadc7b2044
Berke <berke@bucketlist.com.tr>
2020-05-12 15:25:37 +0300

    Updates

---
+++ precache-manifest.7ac7b8c042b80febad09cba1348c37d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0091879104d6e9a44addc7d660b8191",
+    "url": "/itsberkelium.github.io/index.html"
+  },
+  {
+    "revision": "3c6245146e09e4969006",
+    "url": "/itsberkelium.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91282665736a344da9b6",
+    "url": "/itsberkelium.github.io/static/js/2.0ae451f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/itsberkelium.github.io/static/js/2.0ae451f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6245146e09e4969006",
+    "url": "/itsberkelium.github.io/static/js/main.92ffcbdf.chunk.js"
+  },
+  {
+    "revision": "6d2c4d537cfae479e13d",
+    "url": "/itsberkelium.github.io/static/js/runtime-main.f517c1de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/itsberkelium.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
