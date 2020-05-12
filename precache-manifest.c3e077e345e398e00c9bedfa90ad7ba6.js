61f2951a715c2f8cd4436a09e2afb56c3defa7f9
Berke <berke@bucketlist.com.tr>
2020-05-12 15:29:02 +0300

    Updates

---
+++ precache-manifest.c3e077e345e398e00c9bedfa90ad7ba6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d78597f89f6ff7bd724f62e1e1dfdbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25a1bb4bf2b05b4371dc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "91282665736a344da9b6",
+    "url": "/static/js/2.0ae451f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ae451f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a1bb4bf2b05b4371dc",
+    "url": "/static/js/main.0089e698.chunk.js"
+  },
+  {
+    "revision": "4ef113722668580b71d3",
+    "url": "/static/js/runtime-main.09b1badc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
